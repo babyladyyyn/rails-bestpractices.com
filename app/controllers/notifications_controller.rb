ce1d62d855517ec48e24b2f2088e0e3e253c5232
Richard Huang <flyerhzm@gmail.com>
2010-08-25 21:32:17 +0800

    do not eager loading for notifierables

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -15,6 +15,6 @@ class NotificationsController < InheritedResources::Base
     end
     
     def collection
-      @notifications ||= end_of_association_chain.includes(:notifierable => :user).paginate(:page => params[:page], :per_page => Notification.per_page)
+      @notifications ||= end_of_association_chain.paginate(:page => params[:page], :per_page => Notification.per_page)
     end
 end
