3931a303d9dd1c51c91e4b4eb8b4d63426b161a5
Richard Huang <flyerhzm@gmail.com>
2010-08-25 17:49:05 +0800

    move exception_notification configuration from environments into initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,10 +10,10 @@ RailsBestpracticesCom::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
@@ -32,7 +32,7 @@ RailsBestpracticesCom::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -43,4 +43,6 @@ RailsBestpracticesCom::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+  
+  config.active_record.observers = :tweet_observer, :notifier_observer
 end
