53ea43094ddd94e525ff90437e4208dc0505d63a
Richard Huang <flyerhzm@gmail.com>
2010-11-14 17:35:05 +0800

    notification mailer spec

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,5 +1,27 @@
 require "spec_helper"
 
 describe NotificationMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notify_comment" do
+    before :each do
+      @post = Factory(:post, :title => 'email post')
+      @comment = Factory(:comment, :commentable => @post, :body => 'comment body')
+      @email = NotificationMailer.notify_comment("flyerhzm@gmail.com", @comment)
+    end
+
+    it "should be set to be delivered to the email passed in" do
+      @email.should deliver_to("flyerhzm@gmail.com")
+    end
+
+    it "should have the correct subject" do
+      @email.should have_subject(/Comment on Post email post/)
+    end
+
+    it "should contain the comment's body in the mail body" do
+      @email.should have_body_text(/comment body/)
+    end
+
+    it "should contain a link to the post" do
+      @email.should have_body_text(/#{post_url(@post)}/)
+    end
+  end
 end
