a34d279998f7637ae778247d63220a44ace54892
Richard Huang <flyerhzm@gmail.com>
2010-11-21 12:41:10 +0800

    fix mailers.yml syntax, use notification@rails-practices.com

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,17 +1,19 @@
 class NotificationMailer < ActionMailer::Base
-  def load_settings
-    options = YAML.load_file("#{RAILS_ROOT}/config/mailers.yml")[RAILS_ENV]['noreply']
-    @@smtp_settings = {
-      :address              => options["address"],
-      :port                 => options["port"],
-      :domain               => options["domain"],
-      :authentication       => options["authentication"],
-      :user_name            => options["user_name"],
-      :password             => options["password"]
-    }
+  class <<self
+    def smtp_settings
+      options = YAML.load_file("#{RAILS_ROOT}/config/mailers.yml")[RAILS_ENV]['notification']
+      @@smtp_settings = {
+        :address              => options["address"],
+        :port                 => options["port"],
+        :domain               => options["domain"],
+        :authentication       => options["authentication"],
+        :user_name            => options["user_name"],
+        :password             => options["password"]
+      }
+    end
   end
 
-  default :from => "noreply@rails-bestpractices.com"
+  default :from => "notification@rails-bestpractices.com"
 
   def notify_comment(email, comment)
     @comment = comment
