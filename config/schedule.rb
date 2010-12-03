789fba939187cf17d616da089008710ba2816ed2
Richard Huang <flyerhzm@gmail.com>
2010-12-03 21:53:47 +0800

    set output for cron jobs and run cron jobs at different time

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,13 +7,15 @@
 #
 # set :output, "/path/to/my/cron_log.log"
 #
-every 1.day do
+set :output, "/home/huangzhi/sites/rails-bestpractices.com/production/shared/log/cron_log.log"
+
+every 1.day, :at => '2am' do
   rake "sitemap:refresh"
 end
 every 12.hours do
   rake "ts:index"
 end
-every 1.day do
+every 1.day, :at => '1am' do
   rake "backup:run trigger='mysql-backup-dropbox'"
   rake "backup:run trigger='archive-backup-dropbox'"
 end
