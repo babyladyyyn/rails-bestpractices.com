3fe9e71aa4080d22ef076656c887dd77fe060be4
Richard Huang <flyerhzm@gmail.com>
2010-12-21 22:30:39 +0800

    do not put non published post to sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,7 +3,7 @@ SitemapGenerator::Sitemap.default_host = "http://rails-bestpractices.com"
 
 SitemapGenerator::Sitemap.add_links do |sitemap|
   sitemap.add root_path, :priority => 0.7, :changefreq => 'daily'
-  Post.all.each do |p|
+  Post.published.each do |p|
     sitemap.add post_path(p), :lastmod => p.updated_at, :priority => 1.0, :changefreq => 'daily'
   end
   Implementation.all.each do |i|
