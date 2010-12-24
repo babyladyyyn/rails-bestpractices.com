af41b9db9f82cd1651b311a1e5860d7c7de92688
Richard Huang <flyerhzm@gmail.com>
2010-12-25 00:13:20 +0800

    add blog post to sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -7,9 +7,12 @@ SitemapGenerator::Sitemap.add_links do |sitemap|
     sitemap.add post_path(p), :lastmod => p.updated_at, :priority => 1.0, :changefreq => 'daily'
   end
   Page.all.each do |p|
-    sitemap.add page_path(p.name), :lastmod => p.updated_at, :priority => 0.5, :changefreq => 'monthly'
+    sitemap.add page_path(p.name), :lastmod => p.updated_at, :priority => 0.5, :changefreq => 'daily'
   end
   Question.all.each do |q|
     sitemap.add question_path(q), :lastmod => q.updated_at, :priority => 1.0, :changefreq => 'daily'
+  end
+  BlogPost.all.each do |p|
+    sitemap.add post_path(p), :lastmod => p.updated_at, :priority => 1.0, :changefreq => 'daily'
   end
 end
