7688799fe80d66566ab3dde537fdd6eb363638e2
Richard Huang <flyerhzm@gmail.com>
2011-01-15 22:10:16 +0800

    recent blog posts at the sidebar

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,5 +1,6 @@
 class BlogPostsController < InheritedResources::Base
   respond_to :xml, :only => :index
+  before_filter :load_recent_blog_posts
 
   def show
     show! do |format|
@@ -10,5 +11,9 @@ class BlogPostsController < InheritedResources::Base
   protected
     def collection
       @blog_posts ||= end_of_association_chain.order("created_at desc").paginate(:page => params[:page], :per_page => BlogPost.per_page)
+    end
+
+    def load_recent_blog_posts
+      @recent_blog_posts = @blog_posts || BlogPost.order("created_at desc").select("id, title").limit(BlogPost.per_page)
     end
 end
