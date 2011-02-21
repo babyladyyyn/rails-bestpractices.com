d7fa5ad0ef8d5d4ae7451b8862d6cd8c9782779f
Richard Huang <flyerhzm@gmail.com>
2011-02-21 16:39:42 +0800

    ignore .rspec file and revert config.use_transactional_fixtures = true in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,7 +30,7 @@ Spork.prefork do
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, comment the following line or assign false
     # instead of true.
-    # config.use_transactional_fixtures = true
+    config.use_transactional_fixtures = true
 
     config.include(EmailSpec::Helpers)
     config.include(EmailSpec::Matchers)
