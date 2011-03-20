1c9a150bd24bce5def142289973263112b7a0d6e
Richard Huang <flyerhzm@gmail.com>
2011-03-20 22:21:52 +0800

    use devise as SwitchUser provider

--- config/initializers/switch_user.rb
+++ config/initializers/switch_user.rb
@@ -1,5 +1,5 @@
 SwitchUser.setup do |config|
-  config.provider = :authlogic
+  config.provider = :devise
   config.available_users = { :user => lambda { User.all } }
   config.redirect_path = lambda { |request, params| '/' }
 end
