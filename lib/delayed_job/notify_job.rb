1eb0738df55e6cd0835f3d329a66b572a6d2438f
Richard Huang <flyerhzm@gmail.com>
2011-05-07 01:45:04 +0800

    fix typo

--- lib/delayed_job/notify_job.rb
+++ lib/delayed_job/notify_job.rb
@@ -1,6 +1,6 @@
 class DelayedJob::NotifyJob < Struct.new(:job_id)
   def perform
     job = Job.find(job_id)
-    NotificationMailer.notify_admin(job).deliver
+    NotificationMailer.notify_job(job).deliver
   end
 end
