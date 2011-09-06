bf69708d657511fcdac0f961e551640853c2e397
Richard Huang <flyerhzm@gmail.com>
2011-09-06 19:47:48 +0800

    destroy drop model and related controllers

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,6 @@ class Ability
     can :manage, Post, :user_id => user.id
     can :manage, Question, :user_id => user.id
     can :manage, Answer, :user_id => user.id
-    can :manage, Drop, :user_id => user.id
 
     if user.admin?
       can :access, :rails_admin
