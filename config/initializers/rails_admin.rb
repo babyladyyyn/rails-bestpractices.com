68d1875ee0b2f206d7dfee9925975b666dafa5ed
Richard Huang <flyerhzm@gmail.com>
2011-09-25 15:17:48 +0800

    fckeditor support for admin blog post

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,15 @@
 RailsAdmin.config do |config|
   config.authorize_with :cancan
+
+  config.excluded_models << "Authentication"
+
+  config.model BlogPost do
+    edit do
+      field :title
+      field :body do
+        ckeditor true
+      end
+      field :user
+    end
+  end
 end
