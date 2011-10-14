117bef1ce33ae4e528261681b2e70ecd4bbb7e29
Richard Huang <flyerhzm@gmail.com>
2011-10-14 23:36:30 +0800

    remove comments and votes in user/show page, new style applied

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < Devise::RegistrationsController
       params[:nav] = params[:nav] || "posts"
       @children = @user.send(params[:nav])
       @children = @children.published if params[:nav] == "posts"
-      @children = VoteDecorator.decorate(@children) if params[:nav] == "votes"
     end
   end
 
