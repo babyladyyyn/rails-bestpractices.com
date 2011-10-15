bf44fdd73bb05e35235e8c71568dd3217544be10
Richard Huang <flyerhzm@gmail.com>
2011-10-15 11:49:33 +0800

    post decorator

--- app/decorators/notification_decorator.rb
+++ app/decorators/notification_decorator.rb
@@ -5,10 +5,10 @@ class NotificationDecorator < ApplicationDecorator
     notifierable = model.cached_notifierable
     if notifierable.is_a? Comment
       comment = CommentDecorator.new(notifierable)
-      comment.user_link + " commented on " + comment.parent_link
+      (comment.user_link + " commented on " + comment.parent_link).html_safe
     elsif notifierable.is_a? Answer
       answer = AnswerDecorator.new(notifierable)
-      answer.user_link + " answered on Question " + answer.question_link
+      (answer.user_link + " answered on Question " + answer.question_link).html_safe
     end
   end
 end
--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -1,3 +1,31 @@
 class PostDecorator < ApplicationDecorator
   decorates :post
+
+  def statistics
+    h.content_tag(:p, "#{vote_points} votes") +
+      h.content_tag(:p, "#{comments_count} comments") +
+      h.content_tag(:p, "#{view_count} views")
+  end
+
+  def link
+    h.link_to title, h.post_path(self)
+  end
+
+  def implemented_icon
+    if implemented?
+      h.content_tag :span, "implemented", :class => "implemented"
+    end
+  end
+
+  def tag_links
+    links = []
+    TagDecorator.decorate_each(cached_tags) do |tag|
+      links << tag.link
+    end
+    links.join("").html_safe
+  end
+
+  def created_date
+    h.l created_at.to_date
+  end
 end
