f44794e8bff4aa5a581c5d96718b5b0e07a2c8f1
Richard Huang <flyerhzm@gmail.com>
2011-11-26 11:12:24 +0800

    remove wrong db migration

--- db/migrate/20100905013759_add_tag_list_to_drops.rb
+++
@@ -1,9 +0,0 @@
-class AddTagListToDrops < ActiveRecord::Migration
-  def self.up
-    add_column :drops, :tag_list, :string
-  end
-
-  def self.down
-    remove_column :drops, :tag_list
-  end
-end
