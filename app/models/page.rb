3c113ae179b37411568388e79f8e2f9416ae8b9b
Richard Huang <flyerhzm@gmail.com>
2011-12-06 23:00:43 +0800

    add link to title

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,7 +4,7 @@
 #
 #  id         :integer(4)      not null, primary key
 #  name       :string(255)
-#  body       :text(16777215)
+#  body       :text
 #  created_at :datetime
 #  updated_at :datetime
 #
