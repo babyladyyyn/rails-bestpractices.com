5a28b8a190c1fd188ca0ab96b36bf96b8c435ae9
Richard Huang <flyerhzm@gmail.com>
2012-01-04 00:40:10 +0800

    no per_page, use number

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def show
-    @posts = Post.published.search(params[:search], :page => params[:posts_page] || 1, :per_page => Post.per_page)
-    @questions = Question.search(params[:search], :page => params[:questions_page] || 1, :per_page => Question.per_page)
+    @posts = Post.published.search(params[:search], :page => params[:posts_page] || 1, :per_page => 10)
+    @questions = Question.search(params[:search], :page => params[:questions_page] || 1, :per_page => 10)
   end
 end
