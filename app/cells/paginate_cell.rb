30b7e1a145e01d1ce41cfb1df8938ca5d1a12004
Richard Huang <flyerhzm@gmail.com>
2012-01-25 09:36:36 +0800

    3 parameters for paginate_cell#show

--- app/cells/paginate_cell.rb
+++ app/cells/paginate_cell.rb
@@ -1,5 +1,5 @@
 class PaginateCell < Cell::Rails
-  cache :show, :if => lambda { |cell, collection| collection.present? } do |cell, collection|
+  cache :show, :if => lambda { |cell, collection, name=nil| collection.present? } do |cell, collection, name=nil|
     "#{collection.first.model_cache_key}/#{cell.params[:page] || 1}"
   end
 
