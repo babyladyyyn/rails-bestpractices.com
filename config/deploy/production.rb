559a0a910cf04576f90342ae7b5d883f91a1a9cd
Richard Huang <flyerhzm@gmail.com>
2012-02-03 21:45:14 +0800

    different cron jobs for different roles

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -22,12 +22,12 @@ namespace :deploy do
   namespace :update_crontab do
     desc "Update the crontab file on db server"
     task :db, :roles => :db do
-      run "cd #{release_path} && bundle exec whenever --update-crontab #{application} -i config/schedule/db.rb"
+      run "cd #{release_path} && bundle exec whenever --update-crontab -f config/schedule/db.rb -i rails-bestpractices.com-db"
     end
 
     desc "Update the crontab file on app server"
     task :app, :roles => :app do
-      run "cd #{release_path} && bundle exec whenever --update-crontab #{application} -i config/schedule/app.rb"
+      run "cd #{release_path} && bundle exec whenever --update-crontab -f config/schedule/app.rb -i rails-bestpractices.com-app"
     end
   end
 end
