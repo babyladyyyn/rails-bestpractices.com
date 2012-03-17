987bd7febf0939f6a4874102a27e187b7254f30c
Richard Huang <flyerhzm@gmail.com>
2012-03-17 20:38:05 +0800

    add pulish link to admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,7 +2,61 @@ ActsAsTaggableOn::Tag.class_eval do
   attr_accessible :name, :important
 end
 
+module RailsAdmin
+  module Config
+    module Actions
+      class Publish < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+
+        register_instance_option :visible? do
+          bindings[:object].respond_to? :publish!
+        end
+
+        register_instance_option :member do
+          true
+        end
+
+        register_instance_option :http_methods do
+          [:get]
+        end
+
+        register_instance_option :authorization_key do
+          :publish
+        end
+
+        register_instance_option :controller do
+          Proc.new do
+            @object.publish!
+            redirect_to :back
+          end
+        end
+
+        register_instance_option :link_icon do
+          'icon-ok-sign'
+        end
+      end
+    end
+  end
+end
+
 RailsAdmin.config do |config|
+  config.actions do
+    # root actions
+    dashboard                     # mandatory
+    # collection actions
+    index                         # mandatory
+    new
+    export
+    history_index
+    bulk_delete
+    # member actions
+    show
+    edit
+    delete
+    history_show
+    show_in_app
+    publish
+  end
 
   config.current_user_method { current_user } #auto-generated
   config.authorize_with :cancan
@@ -34,6 +88,14 @@ RailsAdmin.config do |config|
       field :published
       field :source
       field :apply_email
+    end
+  end
+
+  config.model Post do
+    list do
+      field :title
+      field :published
+      field :user
     end
   end
 end
