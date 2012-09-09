938cd343cd619bc9ecf6977970f4fc692cda3565
Richard Huang <flyerhzm@gmail.com>
2012-09-09 14:45:19 +0800

    seperate cap tasks to different files

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,57 +21,15 @@ role :web, "app.rails-bestpractices.com"
 role :app, "app.rails-bestpractices.com"
 role :db,  "db.rails-bestpractices.com", :primary => true
 
-before "deploy:assets:precompile", "config:init"
-before "deploy:assets:precompile", "asset:init"
+load "config/deploy/asset_pipeline"
+load "config/deploy/shared_symbolic"
+load "config/deploy/css_sprite"
+
 after "deploy:update_code", "asset:revision"
 
 namespace :asset do
-  task :init, :roles => :app do
-    run "cd #{release_path}; #{rake} RAILS_ENV=#{rails_env} css_sprite:build"
-  end
-
   task :revision, :roles => :app do
     run "cd #{release_path}; git ls-remote origin master | awk '{print $1}' > #{release_path}/public/REVISION"
-  end
-end
-
-set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
-
-namespace :deploy do
-  namespace :assets do
-
-    desc <<-DESC
-      Run the asset precompilation rake task. You can specify the full path \
-      to the rake executable by setting the rake variable. You can also \
-      specify additional environment variables to pass to rake via the \
-      asset_env variable. The defaults are:
-
-        set :rake,      "rake"
-        set :rails_env, "production"
-        set :asset_env, "RAILS_GROUPS=assets"
-        set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
-    DESC
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-
-  end
-end
-
-namespace :config do
-  task :init do
-    run "ln -nfs #{shared_path}/config/bitly.yml #{release_path}/config/bitly.yml"
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/mailers.yml #{release_path}/config/mailers.yml"
-    run "ln -nfs #{shared_path}/config/omniauth.yml #{release_path}/config/omniauth.yml"
-    run "ln -nfs #{shared_path}/config/sphinx.yml #{release_path}/config/sphinx.yml"
-    run "ln -nfs #{shared_path}/config/memcache.yml #{release_path}/config/memcache.yml"
-    run "ln -nfs #{shared_path}/config/initializers/sphinx.rb #{release_path}/config/initializers/sphinx.rb"
   end
 end
 
