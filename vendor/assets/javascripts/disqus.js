64a0b1a2394bf0f55da859550a545c21d4c60fb4
Richard Huang <flyerhzm@gmail.com>
2012-10-25 21:40:01 +0800

    fix typo

--- vendor/assets/javascripts/disqus.js
+++ vendor/assets/javascripts/disqus.js
@@ -1,5 +1,5 @@
 /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-var disqus_shortname = 'raisbestpractices'; // required: replace example with your forum shortname
+var disqus_shortname = 'railsbestpractices'; // required: replace example with your forum shortname
 
 /* * * DON'T EDIT BELOW THIS LINE * * */
 (function() {
