e4304d6a2586c4b33357b91299501ee67c67e092
Richard Huang <flyerhzm@gmail.com>
2012-10-26 13:17:50 +0800

    display blog post comments count in blog index page

--- vendor/assets/javascripts/disqus.js
+++ vendor/assets/javascripts/disqus.js
@@ -3,7 +3,11 @@ var disqus_shortname = 'railsbestpractices'; // required: replace example with y
 
 /* * * DON'T EDIT BELOW THIS LINE * * */
 (function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    var disqus_count = document.createElement('script'); disqus_count.type = 'text/javascript'; disqus_count.async = true;
+    disqus_count.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(disqus_count);
+
+    var disqus_embed = document.createElement('script'); disqus_embed.type = 'text/javascript'; disqus_embed.async = true;
+    disqus_embed.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(disqus_embed);
 })();
