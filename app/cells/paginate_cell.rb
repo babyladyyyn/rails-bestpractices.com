95863a167690d06aed3be4ff031d746c19324261
Richard Huang <flyerhzm@gmail.com>
2012-12-09 15:26:17 +0800

    remove paginate cell cache

--- app/cells/paginate_cell.rb
+++ app/cells/paginate_cell.rb
@@ -1,8 +1,4 @@
 class PaginateCell < Cell::Rails
-  cache :show, :if => lambda { |cell, collection, name=nil| collection.present? } do |cell, collection, name=nil|
-    "#{collection.first.model_cache_key}/#{cell.params[:page] || 1}"
-  end
-
   def show(collection, name=nil)
     if collection.present?
       @collection = collection
@@ -10,5 +6,4 @@ class PaginateCell < Cell::Rails
       render
     end
   end
-
 end
