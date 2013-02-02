5bd9b9665f8a5134d0e4dff04499b2eb30f46468
Richard Huang <flyerhzm@gmail.com>
2013-02-02 13:15:04 +0800

    fix use_try_review plugin

--- lib/rails_best_practices/plugins/reviews/use_try_review.rb
+++ lib/rails_best_practices/plugins/reviews/use_try_review.rb
@@ -8,23 +8,23 @@ module RailsBestPractices
         interesting_nodes :ifop
         interesting_files ALL_FILES
 
-        def start_ifop(node)
+        add_callback :start_ifop do |node|
           conditional_statement = node.conditional_statement
           true_body = node[2]
           second_body = node[3]
           if assert_type?(conditional_statement, :vcall) &&
              assert_type?(true_body, :call) &&
-             str_equal?(conditional_statement, true_body.subject) &&
+             str_equal?(conditional_statement, true_body.receiver) &&
              assert_str?(second_body, "nil")
             # user ? user.name : nil
             add_error("use try (#{conditional_statement})")
           elsif assert_type?(conditional_statement, :call) &&
                 assert_type?(second_body, :call) &&
-                str_equal?(conditional_statement.subject, second_body.subject) &&
+                str_equal?(conditional_statement.receiver, second_body.receiver) &&
                 assert_str?(conditional_statement.message, "nil?") &&
                 assert_str?(true_body, "nil")
             # user.nil? ? nil : user.name
-            add_error("use try (#{conditional_statement.subject})")
+            add_error("use try (#{conditional_statement.receiver})")
           end
         end
 
