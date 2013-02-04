e3e9022b1352c9c883499f00eaea895c71555abd
Richard Huang <flyerhzm@gmail.com>
2013-02-04 10:23:07 +0800

    git ignore session_store

--- config/deploy/recipes/shared_configs.rb
+++ config/deploy/recipes/shared_configs.rb
@@ -4,5 +4,6 @@ namespace :deploy do
   task :update_shared_configs do
     run "ln -nfs #{shared_path}/config/*.yml #{release_path}/config/"
     run "ln -nfs #{shared_path}/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
+    run "ln -nfs #{shared_path}/config/initializers/session_store.rb #{release_path}/config/initializers/session_store.rb"
   end
 end
