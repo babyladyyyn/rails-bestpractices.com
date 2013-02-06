16c0401632d42b227965fdb4914ff943abe30eae
Richard Huang <flyerhzm@gmail.com>
2013-02-06 10:47:31 +0800

    not use Passenger anymore

--- config/application.rb
+++ config/application.rb
@@ -69,15 +69,3 @@ module RailsBestpracticesCom
     config.cache_store = Memcached::Rails.new(MEMCACHE_CONFIG.symbolize_keys.merge(:logger => Rails.logger))
   end
 end
-
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    # Reset Rails's object cache
-    # Only works with DalliStore
-    Rails.cache.reset if forked
-
-    # Reset Rails's session store
-    # If you know a cleaner way to find the session store instance, please let me know
-    # ObjectSpace.each_object(ActionDispatch::Session::DalliStore) { |obj| obj.reset }
-  end
-end
