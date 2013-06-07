914fcc83bdbee6117b470b78dc5559a8e9bb2ce5
Richard Huang <flyerhzm@gmail.com>
2013-06-07 16:33:11 +0800

    remove comments association from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,6 @@ class User < ActiveRecord::Base
   validates :login, :presence => true, :uniqueness => true
 
   has_many :posts, :dependent => :destroy
-  has_many :comments, :dependent => :destroy
   has_many :votes, :dependent => :destroy
   has_many :questions, :dependent => :destroy
   has_many :answers, :dependent => :destroy
