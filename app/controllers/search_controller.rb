a1aca7374d0e24cda1d6e9276a127f6d0b952e06
Richard Huang <flyerhzm@gmail.com>
2013-06-16 13:31:45 +0800

    replace / with whitespace before searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def show
-    @posts = Post.search(params[:search], :page => params[:posts_page] || 1, :per_page => 10)
-    @questions = Question.search(params[:search], :page => params[:questions_page] || 1, :per_page => 10)
+    search = params[:search].gsub("/", " ")
+    @posts = Post.search(search, :page => params[:posts_page] || 1, :per_page => 10)
+    @questions = Question.search(search, :page => params[:questions_page] || 1, :per_page => 10)
   end
 end
