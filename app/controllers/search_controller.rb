57ae46c4bc0a45ea6ed7d34488a5d63d50afa4a9
Richard Huang <flyerhzm@gmail.com>
2013-06-17 08:39:22 +0800

    fix search request when params search doesn't exit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def show
-    search = params[:search].gsub("/", " ")
+    search = params[:search] ? params[:search].gsub("/", " ") : ""
     @posts = Post.search(search, :page => params[:posts_page] || 1, :per_page => 10)
     @questions = Question.search(search, :page => params[:questions_page] || 1, :per_page => 10)
   end
