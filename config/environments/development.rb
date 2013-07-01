4a8c009d310a18699bbfcf7a7a918189d77cb5d9
Richard Huang <flyerhzm@gmail.com>
2013-07-01 23:57:52 +0800

    remove config.active_record.auto_explain_threshold_in_seconds

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,9 +30,6 @@ RailsBestpracticesCom::Application.configure do
 
   config.assets.logger = false
 
-  # Log the query plan for queries taking more than this (works with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   config.action_mailer.default_url_options = { :host => "rails-bestpractices.com" }
 
   config.after_initialize do
